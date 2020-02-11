7882311b9826eb1682ee12d5a662891f8b0caa1a
jyh <jyh6842@gmail.com>
2020-02-11 16:02:27 +0900

    "테이블 수정  . emp, dept PK, FK 생성  테이블 / 컬럼 주석  view 생성  sequence index  . emp pk 제약이 없을때, index가 없을 때  . emp pk 제약을 생성 했을 때(index가 있을 때)  인덱스 구성에 따른 실행계획 비교"

--- 20200211.sql
+++ 20200211.sql
@@ -174,8 +174,175 @@ VIEW는 물리적인 데이터를 갖지 않고, 논리적인 데이터의 정의 집합(SQL)이기 때문에
 VIEW에서 참조하는 테이블의 데이터가 변경이 되면 VIEW의 조회 결과도 영향을 받는다.
 ROLLBACK;
 
+SEQUENCD : 시퀀스 - 중복되지 않는 정수값을 리턴해주는 오라클 객체
+CREATE SEQUENCE 시퀀스 이름
+[OPTION ...]
+명명 규칙 : SEQ_테이블명;
 
- 
- 
+emp 테이블에서 사용한 시퀀스 생성;
 
+CREATE SEQUENCE seq_emp;
+
+시퀀스 제공 함수
+NEXTVAL : 시퀀스에서 다음 값을 가져올 때 사용
+CURRVAL : NEXTVAL를 사용하고나서 현재 읽어 들인 값을 재확인;
+
+SELECT seq_emp.NEXTVAL
+FROM dual;
+
+SELECT *
+FROM emp_test;
+
+INSERT INTO emp_test VALUES (seq_emp.NEXTVAL, 'james', 99, '017');
+
+시퀀스 주의점 
+ROLLBACK을 하더라도 NEXTVAL를 통해 얻은 값이 원복되진 않는다.
+NEXTVAL를 통해 값을 받아오면 그 값을 다시 사용할 수 없다.
+
+
+
+INDEX;
+SELECT ROWID, emp.*
+FROM emp
+WHERE ROWID='AAAE5dAAFAAAACLAAG';
+
+인덱스가 없을때 empno값으로 조회 하는 경우
+emp 테이블에서 pk_emp 제약조건을 삭제하여 empno컬럼으로 인덱스가 존재하지 않는 환경을 조성
+
+ALTER TABLE emp DROP CONSTRAINT pk_emp;
+
+explain plan for
+SELECT *
+FROM emp
+WHERE empno = 7782;
+
+SELECT *
+FROM TABLE(dbms_xplan.display);
+
+ emp 테이블의 empno 컬럼으로 PK 제약을 생성하고 동일한 SQL을 실행
+ PK: UNIQUE + NOT NULL
+     (UNIQUE 인덱스를 생성해준다.)
+ ==> empno 컬럼으로 unique 인덱스가 생성됨
+ 인덱스로 SQL을 실행하게 되면 인덱스가 없을 때와 어떻게 다른지 차이점을 확인;
  
+ ALTER TABLE emp ADD CONSTRAINT pk_emp PRIMARY KEY (empno);
+ 
+ SELECT rowid, emp.*
+ FROM emp;
+ 
+ SELECT empno, rowid
+ FROM emp
+ ORDER BY empno;
+ 
+ explain plan for
+ SELECT *
+ FROM emp
+ WHERE empno = 7782;
+ 
+ SELECT *
+ FROM TABLE(dbms_xplan.display);
+ 
+ INSERT INTO emp VALUES(7369, 'SMITH', 7902, TO_DATE('19801217', 'YYYY/MM/DD'), NULL, 20); -- 않넣어짐
+ 
+ SELECT *
+ FROM emp
+ WHERE ename = 'SMITH';
+ 
+ SELECT 조회컬럼이 테이블 접근에 미치는 영향
+ SELECT * FROM emp WHERE empno = 7782;
+ 
+ SELECT empno FROM emp WHERE empno = 7782;
+ 
+  explain plan for
+ SELECT *
+ FROM emp
+ WHERE empno = 7782;
+ 
+ SELECT *
+ FROM TABLE(dbms_xplan.display);
+ Plan hash value: 2949544139
+ 
+--------------------------------------------------------------------------------------
+| Id  | Operation                   | Name   | Rows  | Bytes | Cost (%CPU)| Time     |
+--------------------------------------------------------------------------------------
+|   0 | SELECT STATEMENT            |        |     1 |    87 |     1   (0)| 00:00:01 |
+|   1 |  TABLE ACCESS BY INDEX ROWID| EMP    |     1 |    87 |     1   (0)| 00:00:01 |
+|*  2 |   INDEX UNIQUE SCAN         | PK_EMP |     1 |       |     0   (0)| 00:00:01 |
+--------------------------------------------------------------------------------------
+ 
+Predicate Information (identified by operation id):
+---------------------------------------------------
+ 
+   2 - access("EMPNO"=7782);
+
+ UNIQUE VS NON-UNIQUE 인덱스의 차이 확인
+ 1. PK_EMP 삭제
+ 2. EMPNO 컬럼으로 NON-UNIQUE 인덱스 생성
+ 3. 실행계획 확인;
+ 
+ ALTER TABLE emp DROP CONSTRAINT pk_emp;
+ CREATE INDEX idx_n_emp_01 ON emp(empno);
+ 
+ explain plan for
+ SELECT *
+ FROM emp
+ WHERE empno = 7782;
+ 
+ SELECT *
+ FROM TABLE(dbms_xplan.display);
+
+Plan hash value: 2778386618
+ 
+--------------------------------------------------------------------------------------------
+| Id  | Operation                   | Name         | Rows  | Bytes | Cost (%CPU)| Time     |
+--------------------------------------------------------------------------------------------
+|   0 | SELECT STATEMENT            |              |     1 |    87 |     2   (0)| 00:00:01 |
+|   1 |  TABLE ACCESS BY INDEX ROWID| EMP          |     1 |    87 |     2   (0)| 00:00:01 |
+|*  2 |   INDEX RANGE SCAN          | IDX_N_EMP_01 |     1 |       |     1   (0)| 00:00:01 |
+--------------------------------------------------------------------------------------------
+ 
+Predicate Information (identified by operation id):
+---------------------------------------------------
+ 
+   2 - access("EMPNO"=7782)
+ 
+Note
+-----
+   - dynamic sampling used for this statement (level=2); 
+   
+ emp 테이블에 job 컬럼을 기준으로 하는 새로운 non-unique 인덱스를 생성
+ CREATE INDEX idx_n_emp02 ON emp (job); //emp로 인덱스 만듬 2번째 인덱스를 활용해서 찾는다. 인덱스를 잘 활용하면 실행 속도가 빠르다.
+ 
+ SELECT job, rowid
+ FROM emp
+ ORDER BY job;
+ 
+ -- 선택가능한 사항
+ 1. emp 테이블을 전체 읽기
+ 2. idx_n_emp_01(empno) 활용
+ 3. idx_n_emp_02(job) 활용; -- 이거 사용하는게 좋다. 내가 원하는 값을 딱 얻을 수 있음 -- 이거 실행할지는 모름
+ explain plan for
+ SELECT *
+ FROM emp
+ WHERE job = 'MANAGER';
+ 
+ select *
+ from table(dbms_xplan.display);
+ Plan hash value: 2645879471
+ 
+-------------------------------------------------------------------------------------------
+| Id  | Operation                   | Name        | Rows  | Bytes | Cost (%CPU)| Time     |
+-------------------------------------------------------------------------------------------
+|   0 | SELECT STATEMENT            |             |     3 |   261 |     2   (0)| 00:00:01 |
+|   1 |  TABLE ACCESS BY INDEX ROWID| EMP         |     3 |   261 |     2   (0)| 00:00:01 |
+|*  2 |   INDEX RANGE SCAN          | IDX_N_EMP02 |     1 |       |     1   (0)| 00:00:01 |
+-------------------------------------------------------------------------------------------
+ 
+Predicate Information (identified by operation id):
+---------------------------------------------------
+ 
+   2 - access("JOB"='MANAGER')
+ 
+Note
+-----
+   - dynamic sampling used for this statement (level=2);
